cac7892b862bd44aa767b45362a2ae665a0dd696
Alex Costinescu <paul.costinescu@gmail.com>
2019-10-08 16:24:37 -0600

    Created Tools

---
+++ src/hardwareStore/ConcreteTool.java
@@ -0,0 +1,18 @@
+package hardwareStore;
+
+public class ConcreteTool extends Tool
+{
+	public ConcreteTool(String name)
+	{
+		super(name);
+	}
+	
+	public int getDailyPrice()
+	{
+		return 15;
+	}
+	public int getCategory()
+	{
+		return CategoryTypes.CONCRETE; 
+	}
+}
