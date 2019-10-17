7b217f3a7f2211b1b941f3bcce251baf4cb6689d
Alex Costinescu <paul.costinescu@gmail.com>
2019-10-17 16:14:13 -0600

    Added print to file option

--- src/hardwareStore/Main.java
+++ src/hardwareStore/Main.java
@@ -1,9 +1,50 @@
 package hardwareStore;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
+		// Ask the user if they want to output to the console or to a file
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Print to console (c) or create a file (f)?");
+		String inputString = "";
+		
+		try
+		{
+			inputString = input.readLine();
+			
+			// Keep asking until the user gives a valid input
+			while(!inputString.equalsIgnoreCase("c") && !inputString.equalsIgnoreCase("f"))
+			{
+				System.out.println("Invalid input: Please input 'c' for console or 'f' for file.");
+				inputString = input.readLine();
+			}
+		} 
+		catch (Exception ex)
+		{
+			System.err.println("An exception occurred while trying to read input:\n" + ex.getStackTrace());
+		}
+		
+		// Output to file if the user input an f
+		Boolean outputToFile = inputString.equalsIgnoreCase("f");
+		if(outputToFile)
+		{
+			try
+			{
+				PrintStream fileStream = new PrintStream(new FileOutputStream("hardwarestore.out"));
+				System.setOut(fileStream);
+			} 
+			catch (Exception ex)
+			{
+				System.err.println("An exception occurred while trying to create an output file:\n" + ex.getStackTrace());
+			}
+		}
+		
 		// Get the World
 		World aWholeNewWorld = World.getTheWorld();
 		
